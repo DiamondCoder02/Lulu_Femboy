2b935cbcb0ee226e82d2fb87494174b2c16f7cc7
DiamondPRO02 <kovacsdanielmark@gmail.com>
2021-11-01 21:56:08 +0100

    / command is retard

--- index.js
+++ index.js
@@ -1,81 +1,24 @@
-const Discord = require('discord.js');
-const config = require('./configuration.json');
-const fs = require('fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
+const commands = [{
+  name: 'ping',
+  description: 'Replies with Pong!'
+}]; 
 
-// Take commands
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-    const command = require(`./commands/` + file);
-    client.commands.set(command.name, command);
-}
+const rest = new REST({ version: '9' }).setToken('token');
 
-// Cooldowns
-const cooldowns = new Discord.Collection();
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
 
-// On Ready
-client.once('ready', () => {
-    console.log('Ready!');
-});
+    await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands },
+    );
 
-// On Message
-client.on('message', message => {
-    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-
-    const args = message.content.slice(config.prefix.length).split(/ +/);
-    const commandName = args.shift().toLowerCase();
-
-    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-    // If command exist
-    if (!command) return;
-
-    // Check if command can be executed in DM
-    if (command.guildOnly && message.channel.type !== 'text') {
-        return message.reply('I can\'t execute that command inside DMs!');
-    }
-
-    // Check if args are required
-    if (command.args && !args.length) {
-        let reply = `You didn't provide any arguments, ${message.author}!`;
-
-        if (command.usage) {
-            reply += `\nThe proper usage would be: \`${config.prefix}${command.name} ${command.usage}\``;
-        }
-
-        return message.channel.send(reply);
-    }
-
-    // Check if user is in cooldown
-    if (!cooldowns.has(command.name)) {
-        cooldowns.set(command.name, new Discord.Collection());
-    }
-
-    const now = Date.now();
-    const timestamps = cooldowns.get(command.name);
-    const cooldownAmount = (command.cooldown || 3) * 1000;
-
-    if (timestamps.has(message.author.id)) {
-        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-        if (now < expirationTime) {
-            // If user is in cooldown
-            const timeLeft = (expirationTime - now) / 1000;
-            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-        }
-    } else {
-        timestamps.set(message.author.id, now);
-        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-        // Execute command
-        try {
-            command.execute(message, args);
-        } catch (error) {
-            console.error(error);
-            message.reply('there was an error trying to execute that command!');
-        }
-    }
-});
-
-client.login(config.token);
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
