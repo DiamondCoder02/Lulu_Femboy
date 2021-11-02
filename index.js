9352d2db3a804ce9cc3e08ccb3ac3bb7e48b9916
DiamondPRO02 <kovacsdanielmark@gmail.com>
2021-11-02 16:13:59 +0100

    fuuuuuuuu

--- index.js
+++ index.js
@@ -22,3 +22,19 @@ const rest = new REST({ version: '9' }).setToken('token');
     console.error(error);
   }
 })();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login('token');
