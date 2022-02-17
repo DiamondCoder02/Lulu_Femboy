8cb60d4e1edae20e21776a9d80cc1a26f9bd93f2
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-02-17 10:22:45 +0100

    -

---
+++ events/ready.js
@@ -0,0 +1,23 @@
+module.exports = (system) => {
+    console.log('Ready!');
+    system.client.user.setActivity("(^///^) Testing / commands")
+        /*
+    "\n\t" + system.lang.ready.login + system.client.user.tag
+    + "\n\n\t" + system.lang.ready.prefix + system.config.prefix
+    + "\n\t" + system.lang.ready.start_lang + system.config.language
+    + "\n\n\t" + system.lang.ready.start + system.client.readyAt
+        )
+        */
+    const ready_embed = new system.Discord.MessageEmbed()
+    .setColor(system.config.embed_colors.blue)
+    .addField("Bot started! \n" 
+    + "Starting prefix:" + `${system.config.prefix}` + "\nLanguage: " + `${system.config.language}`)
+
+    const channel2 = system.client.channels.cache.get(897097242861789236)    
+    const channel = system.client.channels.cache.find(c => c.name === "bot-status")
+    try{
+        channel2.send(ready_embed);return
+    }catch{  
+        channel.send(ready_embed);return
+    }
+}
