d72bebf22819bef63f53872f063c01ff86bc14a4
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-02-22 13:04:58 +0100

    Fuck me

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,14 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		return interaction.reply('Pong!');
+	},
+};
+/*
 module.exports = {
     name: "Ping",
     description: 'Ping test',
@@ -17,4 +28,4 @@ module.exports = {
             )
         })
     }
-}
+}*/
--- index.js
+++ index.js
@@ -1,38 +1,16 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-
-const slash = [
-	{
-		name: 'ping',
-		description: 'Replies with Pong!',
-	},
-];
-
-const rest = new REST({ version: '9' }).setToken('token');
-
-(async () => {
-	try {
-		console.log('Started refreshing application (/) commands.');
-
-		await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: slash });
-
-		console.log('Successfully reloaded application (/) commands.');
-	} catch (error) {
-		console.error(error);
-	}
-})();
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const {Client, Intents} = require('discord.js');
+const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
-});
+  });
 
 client.on('interactionCreate', async (interaction) => {
-	if (!interaction.isCommand()) return;
+  if (!interaction.isCommand()) return;
 
-	if (interaction.commandName === 'ping') {
-		await interaction.reply('Pong!');
-	}
+  if (interaction.commandName === 'ping') {
+    await interaction.reply("pong");
+  }
 });
-client.login('token')
+
+client.login(process.env.TOKEN);
