dc40635627210b7f2a14e41a50a802a5ef9bb516
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-02-24 15:28:01 +0100

    -

--- commands/stop.js
+++ commands/stop.js
@@ -1,11 +1,18 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('stop')
 		.setDescription('Stops the bot with a password.'),
 	async execute(interaction, client) {
-		await interaction.reply(`Bot has been stopped by ${interaction.user.tag}`);
+		await interaction.reply({content: `Bot has been stopped by ${interaction.user.tag}`, ephemeral:true});
 		console.log(`-------------------------\nThe bot is being stopped!!!\nTriggered: ${interaction.user.tag} in #${interaction.channel.name} at ${interaction.createdTimestamp}\n-------------------------`)
+		const embed = new MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle('Embed test')
+			.setURL('https://discord.js.org')
+			.setDescription(`Bot has been stopped by ${interaction.user.tag}`);
+		await interaction.followUp({content: "Stopped!", embeds: [embed]})
 		client.destroy()
 	}
 }
