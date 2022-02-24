74e0c1048435ea4c2424e659a40838f7158cd82b
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-02-24 11:14:36 +0100

    Merge branch 'testing' of https://github.com/DiamondPRO02/Femboi_OwO

--- commands/ping.js
+++ commands/ping.js
@@ -1,31 +1,18 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies Pong'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
-};
-/*
-module.exports = {
-    name: "Ping",
-    description: 'Ping test',
-    cooldown: 10,
-    execute(message, system, cmdArgs) {
-        let date = system.lang.ping.date.split('-')
-		let totalSeconds = (system.client.uptime / 1000);
-		let days = Math.floor(totalSeconds / 86400);
+	async execute(interaction, client) {
+        let totalSeconds = (client.uptime / 1000);
+		//let days = Math.floor(totalSeconds / 86400);
 		let hours = Math.floor( (totalSeconds %= 86400) / 3600);
 		let minutes = Math.floor( (totalSeconds%= 3600) / 60);
 		let seconds = Math.floor(totalSeconds % 60);
-        let uptime = days + date[0] + hours + date[1] + minutes + date[2] + seconds + date[3];
-        
-        message.channel.send("lang.ping.calc").then((resultMessage) => {
-            let ping = resultMessage.createdTimestamp - message.createdTimestamp
-            resultMessage.edit(`Ping: \`` + ping + 'ms\`, Discord API: \`' + system.client.ws.ping + 'ms\`' + "\nUptime:" + uptime
-            )
-        })
-    }
-}*/
+        let uptime = hours + ":" + minutes + ":" + seconds
+        const msg = await interaction.reply({content: "Calculating ping...", fetchReply:true});
+        ping = msg.createdTimestamp - interaction.createdTimestamp;
+        await interaction.editReply(`Ping: \`` + ping + 'ms\`, Discord API: \`' + client.ws.ping + 'ms\`' + "\nUptime:" + uptime)
+		
+	}
+}
--- commands/stop.js
+++ commands/stop.js
@@ -4,41 +4,8 @@ module.exports = {
 		.setName('stop')
 		.setDescription('Stops the bot with a password.'),
 	async execute(interaction, client) {
-		await interaction.reply('STOP!');
+		await interaction.reply(`Bot has been stopped by ${interaction.user.tag}`);
+		console.log(`-------------------------\nThe bot is being stopped!!!\nTriggered: ${interaction.user.tag} in #${interaction.channel.name} at ${interaction.createdTimestamp}\n-------------------------`)
 		client.destroy()
-		.then(() => process.exit())
 	},
 };
-/*
-const Discord = require("discord.js")
-module.exports = {
-    name: 'stop',
-	guildOnly: true,
-	permissions: "ADMINISTRATOR",
-    args: true,
-	usage: '<password>',
-	execute(message, system, args) {	
-		const stop_embed = new system.Discord.MessageEmbed()
-		if(args[0] === system.config.bot_password){
-			try{
-				stop_embed
-				.setColor(system.config.embed_colors.important)
-				.addField(system.lang.stop.end_message, message.author)
-				if (message.channel.type != 'dm') message.delete()
-				message.channel.send(stop_embed)
-				.then(() => system.client.destroy())
-				.then(() => process.exit())
-			} catch (error) {
-				stop_embed
-				.setColor(system.config.embed_colors.error)
-				.addField(message.author, `\`${error.message}\``)
-				message.channel.send(error_stop_embed);
-			}
-		}else{
-			stop_embed
-			.setColor(system.config.embed_colors.error)
-			.addField(message.author.tag, `\`${system.lang.reboot.wrong_pass}\``)
-			message.channel.send(stop_embed);
-		}
-    }
-}*/
