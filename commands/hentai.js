75ec3fc6f5dfc05a9180a8dac73189084d67443a
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-03-06 22:01:30 +0100

    -

--- commands/hentai.js
+++ commands/hentai.js
@@ -3,10 +3,8 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('hentai')
-		.setDescription('For horny people some horny pictures from rule34')
-		.addStringOption(option => option.setName('fetish').setDescription('Give me something to search for.').setRequired(true)),
+		.setDescription('For horny people some horny pictures'),
 	async execute(interaction) {
-        const args = interaction.options.getString('fetish')
         
 	},
 };
