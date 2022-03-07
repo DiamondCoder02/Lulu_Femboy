2756368a85482a0159eb5e8f45e8d7a776410f51
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-03-07 11:59:01 +0100

    It just works

--- commands/nekoslife_nsfw.js
+++ commands/nekoslife_nsfw.js
@@ -15,23 +15,13 @@ module.exports = {
                 .setRequired(true)
         ),
 	async execute(interaction) {
-            const sfw_option = interaction.options.getString('category')
-            console.log(sfw_option)
-            console.log("Fuck0")
-            if (sfw_option === 'hentai') {
-                console.log("Fuck1")
-                lewd = await neko.nsfw.hentai()
-                console.log("Fuck2")
-            }else if (interaction.options.getString() === 'femdom') {
-                const lewd = await neko.nsfw.femdom()
-            }else if (interaction.options.getString() === 'futa') {
-                const lewd = await neko.nsfw.futa()
-            }
-            console.log("Fuck3")
-            //const lewd = await neko.sfw.hug()
+            if (interaction.options.getString('category') === 'hentai') {lewd = await neko.nsfw.hentai()}
+            if (interaction.options.getString('category') === 'femdom') {lewd = await neko.nsfw.femdom()}
+            if (interaction.options.getString('category') === 'futa') {lewd = await neko.nsfw.futanari()}
+
             const embed = new MessageEmbed()
                 .setColor('#00FF00')
-                .setTitle('UwU')
+                .setTitle('UwU, '+ interaction.options.getString('category'))
                 .setTimestamp()
                 .setFooter({ text: 'FembOwO#3146' })
                 .setImage(lewd.url)
