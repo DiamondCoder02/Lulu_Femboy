1b9e48a82fab0642586bf2e5b87a9e0809a81a86
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-03-07 18:04:27 +0100

    -

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,14 +2,18 @@ module.exports = {
 	name: 'interactionCreate',
 	execute(interaction) {
 		const i = interaction
-		if (i.isCommand()) {
-			console.log(`[${i.createdAt}] -- ${i.user.tag} in ${i.guild.name} at #${i.channel.name} triggered: ${i.commandName}.`);
-		}
-		else if (i.isButton()) {
-			console.log(`[${i.createdAt}] -- ${i.user.tag} in ${i.guild.name} at #${i.channel.name} triggered a button.`);
-		} 
-		else if (i.isSelectMenu()) {
-			console.log(`[${i.createdAt}] -- ${i.user.tag} in ${i.guild.name} at #${i.channel.name} triggered a select menu.`);
+		try{
+			if (i.isCommand()) {
+				console.log(`[${i.createdAt}] -- ${i.user.tag} in ${i.guild.name} at #${i.channel.name} triggered: ${i.commandName}.`);
+			}
+			else if (i.isButton()) {
+				console.log(`[${i.createdAt}] -- ${i.user.tag} in ${i.guild.name} at #${i.channel.name} triggered a button.`);
+			} 
+			else if (i.isSelectMenu()) {
+				console.log(`[${i.createdAt}] -- ${i.user.tag} in ${i.guild.name} at #${i.channel.name} triggered a select menu.`);
+			}
+		} catch (error) {
+			console.log(error)
 		}
 	}
 };
