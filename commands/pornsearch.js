b84cb8d91f28b5eed862ce2be00f32e11807cdf1
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-03-10 18:20:22 +0100

    fuck...

--- commands/pornsearch.js
+++ commands/pornsearch.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
+const Pornsearch = require('pornsearch');
 module.exports = {
 	data: new SlashCommandBuilder()
         .setName('pornsearch')
@@ -10,32 +11,38 @@ module.exports = {
             .addStringOption(option =>
                 option.setName('video')
                 .setDescription('Choose which you want from')
-                .addChoice('Pornhub', 'phub')
+                .addChoice('Pornhub', 'pornhub')
                 .addChoice('Sex.com', 'sex')
-                .addChoice('Redtube', 'red')
-                .addChoice('Xvideos', 'xvid')
-                .setRequired(true)
-            ))
+                .addChoice('Redtube', 'redtube')
+                .addChoice('Xvideos', 'xvideos')
+                .setRequired(true))
+            .addStringOption(option => option.setName('search_video_term').setDescription('Search for something').setRequired(true))
+            .addIntegerOption(option => option.setName('repeat').setDescription('Amount: If you want to get more than one at a time.')))
         .addSubcommand(subcommand => subcommand
             .setName('gifs')
             .setDescription('Gifs search (pornhub.com(.webm), sex.com(.gif))')
             .addStringOption(option =>
                 option.setName('gif')
                 .setDescription('Choose which you want from')
-                .addChoice('Pornhub', 'ph_gif')
-                .addChoice('Sex.com', 'sex_gif')
-                .setRequired(true)
-            )),
+                .addChoice('Pornhub', 'pornhub')
+                .addChoice('Sex.com', 'sex')
+                .setRequired(true))
+            .addStringOption(option =>option.setName('search_gif_term').setDescription('Search for something').setRequired(true))
+            .addIntegerOption(option => option.setName('repeat').setDescription('Amount: If you want to get more than one at a time.'))),
     //Execution
     async execute(interaction, client, config) {
         if (!interaction.channel.nsfw) {
             interaction.reply('Sorry, this is a Not Safe For Work command!'); return;
         }
         if (interaction.options.getSubcommand() === 'videos') {
-
+            if (interaction.options.getString('video') === 'pornhub') {
+                
+            }
         }
         if (interaction.options.getSubcommand() === 'gifs') {
-
+            if (interaction.options.getString('gif') === 'pornhub') {
+                
+            }
         }
     }
 }
