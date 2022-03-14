d5c6686bcdb25a2e63432e7c97cadc7beefdbaf8
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-03-14 14:29:03 +0100

    fuck

--- commands/test.js
+++ commands/test.js
@@ -1,126 +1,81 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageActionRow, MessageButton, MessageEmbed, MessageSelectMenu } = require('discord.js');
+const {MessageActionRow,MessageButton,MessageEmbed} = require('discord.js')
 module.exports = {
 	data: new SlashCommandBuilder()
-        .setName('test')
-        .setDescription('Feature testing.'),
-    async execute(interaction, client, config) {
-        const row = new MessageActionRow()
-		.addComponents(
-			new MessageButton()
-				.setCustomId('primary')
-				.setLabel('Primary')
-				.setStyle('PRIMARY'),
-		)
-		.addComponents(
-			new MessageButton()
-				.setCustomId('secondary')
-				.setLabel('Secondary')
-				.setStyle('SECONDARY'),
-		)
-		.addComponents(
-			new MessageButton()
-				.setCustomId('success')
-				.setLabel('Success')
-				.setStyle('SUCCESS'),
-		)
-		.addComponents(
-			new MessageButton()
-				.setCustomId('danger')
-				.setLabel('Danger')
-				.setStyle('DANGER'),
-		)
-		.addComponents(
-			new MessageButton()
-				.setURL('https://discordjs.guide/interactions/buttons.html')
-				.setLabel('link')
-				.setStyle('LINK'),
-		)
-        const row2 = new MessageActionRow()
-		.addComponents(
-			new MessageButton()
-				.setCustomId('one')
-				.setLabel('Right')
-				.setStyle('PRIMARY'),
-		)
-		.addComponents(
-			new MessageButton()
-				.setCustomId('two')
-				.setLabel('Left')
-				.setStyle('SECONDARY'),
-		)
-		.addComponents(
-			new MessageButton()
-				.setCustomId('yay')
-				.setLabel('Yay')
-				.setStyle('SUCCESS'),
-		)
-		.addComponents(
-			new MessageButton()
-				.setCustomId('no')
-				.setLabel('Bruh')
-				.setStyle('DANGER'),
-		)
-		.addComponents(
-			new MessageButton()
-				.setURL('https://discordjs.guide/')
-				.setLabel('link: Discordjs.guide')
-				.setStyle('LINK'),
-		)
-		const menu = new MessageActionRow()
-		.addComponents(
-			new MessageSelectMenu()
-				.setCustomId('select')
-				.setPlaceholder('Nothing selected')
-				.addOptions([
-					{
-						label: 'Select me',
-						description: 'This is a description',
-						value: 'first_option1',
-					},
-					{
-						label: 'You can select me too',
-						description: 'This is also a description',
-						value: 'second_option1',
-					},
-				]),
-		);
-		const menu2 = new MessageActionRow()
-		.addComponents(
-			new MessageSelectMenu()
-				.setCustomId('select2')
-				.setPlaceholder('Nothing selected')
-				.setMinValues(2)
-				.setMaxValues(3)
-				.addOptions([
-					{
-						label: 'Select me',
-						description: 'This is a description',
-						value: 'first_option2',
-					},
-					{
-						label: 'You can select me too',
-						description: 'This is also a description',
-						value: 'second_option2',
-					},
-					{
-						label: 'I am also an option',
-						description: 'This is a description as well',
-						value: 'third_option2',
-					},
-				]),
-		);
-		const embed = new MessageEmbed()
-        .setColor('#00FF00')
-        .setTitle("Wut?")
-        .setDescription(`BingBong`)
-        .setThumbnail(client.user.displayAvatarURL())
-        .addFields(
-            { name: "Command:", value: "Bruh", inline:true},
-            { name: "Idea:", value: "Head empty", inline:true}
-		)
-        .setTimestamp()
-        .setFooter({text: `Ja`});
-        await interaction.reply({content: "Testing", embeds: [embed], components: [row, row2, menu, menu2]})
-    }
+		.setName('test')
+		.setDescription('Feature testing.'),
+	async execute(interaction, client, config) {
+		const nHentai = require('shentai')
+        const sHentai = new nHentai
+		const doujins = await sHentai.getNew()
+		// Constants
+		const backId = 'back'
+		const forwardId = 'forward'
+		const backButton = new MessageButton({
+			style: 'SECONDARY',
+			label: 'Back',
+			emoji: '⬅️',
+			customId: backId
+		})
+		const forwardButton = new MessageButton({
+			style: 'SECONDARY',
+			label: 'Forward',
+			emoji: '➡️',
+			customId: forwardId
+		})
+		// Put the following code wherever you want to send the embed pages:
+		const {author,channel} = interaction
+		const guild = [...client.guilds.cache.values()]
+		/**
+		 * Creates an embed with guilds starting from an index.
+		 * @param {number} start The index to start from.
+		 * @returns {Promise<MessageEmbed>}
+		 */
+		
+		const generateEmbed = async start => {
+			const current = doujins.slice(start, start + 10)
+			// You can of course customise this embed however you want
+			return new MessageEmbed({
+				title: `Showing guilds ${start + 1}-${start + current.length} out of ${doujins.length}`,
+				fields: await Promise.all(
+					current.map(async doujins => ({
+						name: doujins.id,
+						value: `${doujins.titles.english}`
+					}))
+				)
+			})
+		}
+		// Send the embed with the first 10 guilds
+		const canFitOnOnePage = doujins.length <= 10
+		const embedMessage = await channel.send({
+			embeds: [await generateEmbed(0)],
+			components: canFitOnOnePage ?
+				[] :
+				[new MessageActionRow({
+					components: [forwardButton]
+				})]
+		})
+		// Exit if there is only one page of guilds (no need for all of this)
+		if (canFitOnOnePage) return
+		// Collect button interactions (when a user clicks a button),
+		// but only when the button as clicked by the original message author
+		const collector = embedMessage.createMessageComponentCollector()
+		let currentIndex = 0
+		collector.on('collect', async interaction => {
+			// Increase/decrease index
+			interaction.customId === backId ? (currentIndex -= 10) : (currentIndex += 10)
+			// Respond to interaction by updating message with new embed
+			await interaction.update({
+				embeds: [await generateEmbed(currentIndex)],
+				components: [
+					new MessageActionRow({
+						components: [
+							// back button if it isn't the start
+							...(currentIndex ? [backButton] : []),
+						]
+					})
+				]
+			})
+		})
+	}
 }
