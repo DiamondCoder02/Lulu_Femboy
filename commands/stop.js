29da27871ff960082adf755795799ab1fbfe517f
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-03-26 20:30:24 +0100

    Language part3 with my broken german and curse OwO

--- commands/stop.js
+++ commands/stop.js
@@ -4,24 +4,25 @@ module.exports = {
 		.setName('stop')
 		.setDescription('Stops the bot with a password.')
 		.addStringOption(option => option.setName('stoppassword').setDescription('Enter a password to stop the bot').setRequired(true)),
-	async execute(interaction, client, config) {
-		const channel = client.channels.cache.get(config.bot_status_channelId);
+	async execute(interaction, client, config, lang) {
+		let st = lang.stop.split('-')
 		if (config.stopPassword === interaction.options.getString('stoppassword')){
 			console.log(`-------------------------\nThe bot has stopped!!!`)
 			console.log(`Triggered: ${interaction.user.tag} in ${interaction.guild.name}, #${interaction.channel.name} at ${interaction.createdAt}\n-------------------------`)
 			const embed = new MessageEmbed()
 				.setColor('#ff0000')
-				.setTitle('Bot stopped!')
-				.setDescription(`Bot has been stopped by ${interaction.user.tag}`)
-			await interaction.reply({content: "Stopped! :3", ephemeral: true})
-			await interaction.followUp({content: "Stopped!", embeds: [embed]})
+				.setTitle(st[0])
+				.setDescription(st[1] + interaction.user.tag)
+			await interaction.reply({content: st[0], ephemeral: true})
+			await interaction.followUp({embeds: [embed]})
 			client.destroy()
 		} else {
 			const embed = new MessageEmbed()
 				.setColor('#0099ff')
-				.setTitle('Wrong password')
-				.setDescription(`You gave a wrong password ${interaction.user.tag}.\n`)
-			await interaction.reply({embeds: [embed]})
+				.setTitle(st[2])
+				.setDescription(st[3] + interaction.user.tag)
+				await interaction.reply({content: st[2], ephemeral: true})
+				await interaction.followUp({embeds: [embed]})
 		}
 	}
 }
