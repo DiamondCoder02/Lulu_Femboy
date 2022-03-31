d0b9898ab4075f99304722687a98567a5e48fa9c
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-03-31 14:50:23 +0200

    works without embed

--- commands/translate.js
+++ commands/translate.js
@@ -5,10 +5,10 @@ module.exports = {
     cooldown: 5,
 	data: new SlashCommandBuilder()
 		.setName('translate')
-		.setDescription('Translate messages to a language')
-        .addStringOption(option => option.setName('to').setDescription('Translate to').setRequired(true))
+		.setDescription('Translate messages to a language. Please choose languages in English')
         .addStringOption(option => option.setName('text').setDescription('The text that needs translation').setRequired(true))
-		.addStringOption(option => option.setName('from').setDescription('Translate from')),
+		.addStringOption(option => option.setName('from').setDescription('Translate from (Default English)'))
+        .addStringOption(option => option.setName('to').setDescription('Translate to (Default English)')),
     async execute(interaction, client, config, lang) {
         try {
             translate.engine = "google"; // Or "yandex", "libre", "deepl"
@@ -20,7 +20,8 @@ module.exports = {
                 .setTitle(String(text))
                 .setDescription("translated from: \n"+interaction.options.getString('text'))
             await interaction.reply({content: text + "\ntranslated from: \n"+interaction.options.getString('text')})
-            //interaction.reply({embeds: embed})
+            console.log(embed)
+            //await interaction.reply({embeds: embed})
         } catch { interaction.reply({content: "Error: please make sure you wrote the languages correctly."}) }
     }
 }
