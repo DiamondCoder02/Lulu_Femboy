ab2167b7a7406c57e252e4d159b80ffe9dd7ec0b
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-03-31 19:17:49 +0200

    -___-

--- commands/translate.js
+++ commands/translate.js
@@ -1,8 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders'), { MessageEmbed } = require('discord.js');
-//const translate = require('@iamtraction/google-translate');
 const translate = require('translate')
 module.exports = {
-    cooldown: 5,
+    cooldown: 10,
 	data: new SlashCommandBuilder()
 		.setName('translate')
 		.setDescription('Translate messages to a language. Please choose languages in English')
@@ -10,20 +9,18 @@ module.exports = {
 		.addStringOption(option => option.setName('from').setDescription('Translate from (Default English)'))
         .addStringOption(option => option.setName('to').setDescription('Translate to (Default English)')),
     async execute(interaction, client, config, lang) {
-        //try {
+        if (interaction.options.getString('from') === null) { from = "english"} else from = interaction.options.getString('from')
+        if (interaction.options.getString('to') === null) { to = "english"} else to = interaction.options.getString('to')
+        try {
             translate.engine = "google"; // Or "yandex", "libre", "deepl"
             translate.key = process.env.GOOGLE_KEY;
-            const text = await translate(interaction.options.getString('text'), { from: interaction.options.getString('from'), to: interaction.options.getString('to') });
-
+            const text = await translate(interaction.options.getString('text'), {cache: 10000, from: from, to: to });
             const embed = new MessageEmbed()
                 .setColor('#00ffff')
-                .setTitle("Translation: "+ interaction.options.getString('from') + " -> " + interaction.options.getString('to'))
-                .setDescription(text)
-                .setTimestamp()
-                .addField("translated from:", interaction.options.getString('text'))
-            //await interaction.reply({content: text + "\ntranslated from: \n"+interaction.options.getString('text')})
-            console.log(embed)
+                .setTitle("Translation from google: \n"+ from + " -> " + to)
+                .setDescription("--------------------\n"+text+"\n--------------------")
+                .addField("translated from:", "*"+interaction.options.getString('text')+"*")
             await interaction.reply({embeds: [embed]})
-        //} catch { interaction.reply({content: "Error: please make sure you wrote the languages correctly."}) }
+        } catch { interaction.reply({content: "Error: please make sure you wrote the languages correctly."}) }
     }
 }
