4875a780abe10f65d23f61e1b8c0351af7e840fe
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-04-06 13:43:35 +0200

    -__-

--- commands/booru.js
+++ commands/booru.js
@@ -1,79 +1,62 @@
 //lewd command (Gihub Copilot)
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js')
-const Booru = require('booru')
-const { BooruError } = require('booru')
+const { SlashCommandBuilder } = require('@discordjs/builders'), { MessageEmbed } = require('discord.js')
+const wait = require('node:timers/promises').setTimeout;
+const Booru = require('booru'), { BooruError } = require('booru')
 module.exports = {
 	//guildOnly: true,
 	cooldown: 1,
     data: new SlashCommandBuilder()
         .setName('booru')
-        .setDescription('Search rule34 for a picture')
+        .setDescription('Search booru for a picture')
         .addStringOption(option => option.setName('sites').setDescription('Sites to search')
-            .addChoice('e621.net', 'e621')
-            .addChoice('e926.net (wholesome)', 'e926')
-            .addChoice('hypnohub.net', 'hypnohub')
-            .addChoice('danbooru.donmai.us', 'danbooru')
-            .addChoice('konachan.com', 'konac')
-            .addChoice('konachan.net (wholesome)', 'konan')
-            .addChoice('yande.re', 'yandere')
-            .addChoice('gelbooru.com', 'gelbooru')
-            .addChoice('rule34.xxx', 'rule34')
-            .addChoice('safebooru.org (wholesome)', 'safebooru')
-            .addChoice('tbib.org (wholesome)', 'tbib')
-            .addChoice('xbooru.com', 'xbooru')
-            .addChoice('rule34.paheal.net', 'paheal')
-            .addChoice('derpibooru.org', 'derpibooru')
-            .addChoice('realbooru.net', 'realbooru')
+            .addChoice('e621.net 18+', 'e621')
+            .addChoice('e926.net <18', 'e926')
+            .addChoice('hypnohub.net 18+', 'hypnohub')
+            .addChoice('danbooru.donmai.us 18+', 'danbooru')
+            .addChoice('konachan.com 18+', 'konac')
+            .addChoice('konachan.net <18', 'konan')
+            .addChoice('yande.re 18+', 'yandere')
+            .addChoice('gelbooru.com 18+', 'gelbooru')
+            .addChoice('rule34.xxx 18+', 'rule34')
+            .addChoice('safebooru.org <18', 'safebooru')
+            .addChoice('tbib.org <18', 'tbib')
+            .addChoice('xbooru.com 18+', 'xbooru')
+            .addChoice('rule34.paheal.net 18+', 'paheal')
+            .addChoice('derpibooru.org 18+', 'derpibooru')
+            .addChoice('realbooru.net 18+', 'realbooru')
             .setRequired(true)
         )
-        .addStringOption(option => option.setName('tags').setDescription('Tags to search for').setRequired(true)),
-        //one day I will do this: .addNumberOption(option => option.setName('repeat').setDescription('Amount: If you want to get more than one at a time.').setMinValue(1).setMaxValue(10)),
+        .addStringOption(option => option.setName('tags').setDescription('Tags to search for').setRequired(true))
+        .addNumberOption(option => option.setName('repeat').setDescription('Amount: If you want to get more than one at a time.').setMinValue(1).setMaxValue(10)),
     async execute(interaction, client, config, lang) {
-    try{
         if (!interaction.channel.nsfw && interaction.channel.type === 'GUILD_TEXT') { interaction.reply(lang.nsfw); return }
         const sites = interaction.options.getString('sites')
         const tags = interaction.options.getString('tags')
-        //const repeat = interaction.options.getNumber('repeat')
         //console.log(sites + "\n" + tags + "\n" + repeat)
-        // Search with promises
-        Booru.search(sites, tags, { limit: 1, random: true }).then(posts => {
-            for (let post of posts) {
-                // console.log(post + "\n ------------------------------------------- \n" + posts)
-                //embed
+        if (interaction.options.getNumber('repeat')) { var amount = Number(interaction.options.getNumber('repeat')) } else var amount = 1
+        for (let a = 0; a < amount; ) {
+            async function booruSearch(sites, tags, limit = 1, random = true) {
+                const posts = await Booru.search(sites, tags, {limit, random})
                 const embed = new MessageEmbed()
                     .setTitle(sites)
                     .setColor('#ff0000')
                     .setDescription('Searching for ' + tags + ' on ' + sites)
-                    .setImage(post.fileUrl)
+                    .setImage(posts[0].fileUrl)
                     .setTimestamp()
                 try{
-                    interaction.reply({embeds: [embed]})
-                    console.log(post.fileUrl)
+                    await interaction.reply({embeds: [embed]})
+                    console.log(posts[0].fileUrl)
                 }catch{
-                    interaction.followUp({embeds: [embed]})
-                    console.log(post.fileUrl)
+                    await wait(1000)
+                    await interaction.followUp({embeds: [embed]})
+                    console.log(posts[0].fileUrl)
                 }
             }
-        }).catch(err => {
-            if (err instanceof BooruError) {
-            // It's a custom error thrown by the package
-            // Typically results from errors the boorus returns, eg. "too many tags"
-                console.error(err)
-            } else {
-            // This means something pretty bad happened
-                console.error(err)
-            }
-        })
-        /*
-        async function booruSearch(sites, tags, limit = 1, random = true) {
-            try{
-            const posts = await Booru.search(sites, tags, {limit, random})
-            return console.log(posts[0].fileUrl)
-            } catch (err) { console.log(err) }
+            booruSearch(sites, tags, 1, true).catch(err => { 
+                if (err instanceof BooruError) { console.error(err) } 
+                else { interaction.reply({content: "Something went wrong. Make sure you wrote the tag and chosen the correct site."});console.error(err) }
+            })
+            a+=1
         }
-        booruSearch(sites, tags, 1, true)
-        */
-    } catch (err) { console.log(err) }
     }
 };
