e6446643d0ccfcd9cbd46955c60133a069b80fd5
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-05-17 11:39:01 +0200

    -

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require('discord.js');
 const {language} = require('../config.json'), lang = require('../languages/' + language + '.json')
 module.exports = {
 	name: 'guildDelete',
-	execute(client) {
-        console.log(`Bot left guild: ${client.guild.name}`)
+	execute(guild) {
+        console.log(`Bot left guild: ${guild.name}`)
 	}
 };
