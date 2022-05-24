af5ed94dd7905e9aa136d15d380618a91de7f01b
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-05-24 11:36:06 +0200

    sticker

--- events/stickerCreate.js
+++ events/stickerCreate.js
@@ -2,8 +2,9 @@ const { MessageEmbed } = require('discord.js');
 const {language} = require('../config.json'), lang = require('../languages/' + language + '.json')
 module.exports = {
 	name: 'stickerCreate',
-	execute(sticker) {
-		console.log(sticker)
-        console.log(`Guild sticker created: ${sticker.name}`)
+	async execute(sticker, client) {
+		//console.log(sticker)
+        const gu = await client.guilds.fetch(sticker.guildId)
+		console.log(`${gu.name} guild sticker created: "${sticker.name}.${sticker.format}" [Desc: ${sticker.description} / Tags: ${String(sticker.tags)}] (ID: ${sticker.id})`)
 	}
 };
--- events/stickerDelete.js
+++ events/stickerDelete.js
@@ -2,8 +2,9 @@ const { MessageEmbed } = require('discord.js');
 const {language} = require('../config.json'), lang = require('../languages/' + language + '.json')
 module.exports = {
 	name: 'stickerDelete',
-	execute(sticker) {
-		console.log(sticker)
-        console.log(`Guild sticker deleted: ${sticker.name}`)
+	async execute(sticker, client) {
+		//console.log(sticker)
+		const gu = await client.guilds.fetch(sticker.guildId)
+		console.log(`${gu.name} guild sticker deleted: "${sticker.name}.${sticker.format}" [Desc: ${sticker.description} / Tags: ${String(sticker.tags)}] (ID: ${sticker.id})`)
 	}
 };
