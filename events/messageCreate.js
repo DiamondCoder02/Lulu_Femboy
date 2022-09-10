f8b3b7451f835f740c50fcabade67a8acff18166
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-09-10 02:12:35 +0200

    -

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -12,22 +12,7 @@ module.exports = {
 		if (message.content.toLowerCase().includes("can i fuck") && message.content.toLowerCase().includes("bot") ) { message.react('<:what_how:961926449806315530>'); } 
 		if (message.content.toLowerCase().includes("suck my horse cock") ) { message.channel.send('<:horse1:973550284041232414><:horse2:973550337711558656><:horse3:973550350449639424><:horse4:973550361505824778>') }
 		if (message.content.toLowerCase().includes("suck my giant horse cock") ) { message.channel.send('<:horse1:973550284041232414><:horse2:973550337711558656><:horse3:973550350449639424><:horse3:973550350449639424><:horse3:973550350449639424><:horse3:973550350449639424><:horse3:973550350449639424><:horse3:973550350449639424><:horse4:973550361505824778>') }
-		// Guild specific reply (Mainly inside jokes)
-		if (message.guildId === "953982453436018748") { 
-			if (message.content === "🥛" || message.content.toLowerCase() === "milk" || message.content.toLowerCase() === "tej" ) { message.channel.send("🥛 for President!"); }
-			if (message.content.toLowerCase().includes("bargo")) { message.channel.send("https://cdn.discordapp.com/attachments/1003357797804691596/1010658560759300196/Screenshot_20220820_231521.jpg"); }
-			if (message.content.toLowerCase() === "tea" || message.content.toLowerCase() === "tea :3" ) { message.react('🍵'); }
-			if (message.content.toLowerCase() === '🍵' ) { message.channel.send('https://tenor.com/view/ggeezy-ggiorno-coffee-morning-gif-13580806'); }
-			if (message.content.toLowerCase().includes("uwu") ) { message.react('<:uwu:957683216393855006>'); }
-			if (message.content.toLowerCase().includes("owo") ) { message.react('<a:cute_uwu:968533677560717403>'); }
-			if (message.content.toLowerCase().includes("faszom java") || message.content.toLowerCase() === "katsumi" ) { message.channel.send({stickers: ['998944167071846510']}) } 
-			if (message.content.toLowerCase().includes("három") || message.content.toLowerCase().includes("three") ) { message.channel.send({stickers: ['967830438070595614']}) } 
-			if (message.content.toLowerCase().includes("nohorny") || (message.content.toLowerCase().includes("horny") && (message.content.toLowerCase().includes("nincs") || message.content.toLowerCase().includes("stop") || message.content.toLowerCase().includes("no"))) ) { message.channel.send({stickers: ['1008492797013278740']}) } 
-			if (message.content.includes("Ki korán kell, aranyat lel. Viszont aki később az femboy-t talál") ||  message.content.includes("Ki korán kell, aranyat lel. *Viszont aki később az femboy-t talál*")) { message.channel.send("My master and creator: https://ppl.moe/u/diamondcoder"); }
-		}
-		if (message.guildId === "893201403756052521") { 
-			if (message.content.toLowerCase().includes("always horny") || message.content.toLowerCase() === "diamond" ) { message.channel.send({stickers: ['1008706454842261566']}) } 
-		}
+		
 		//Must last as I'm lazy
         if (!message.mentions.has(client.user)) return;
         if (message.mentions.users.first().id === client.user.id && message.content === "<@" + client.user.id + ">") { return message.channel.send(`
