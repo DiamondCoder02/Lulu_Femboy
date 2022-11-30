84b18d0232033ffb65fc63052ea7c718eabbce7f
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-11-30 12:06:12 +0100

    -

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -35,27 +35,31 @@ module.exports = {
 				const sStic = message.stickers.map(Stickers => Stickers.name).join(", ");
 				embed.addFields({ name: "Sticker name:", value: sStic })
 			}
-			// Due to v14 Message_Delete type is now number 72
-			let fetchedLogs = await message.guild.fetchAuditLogs({ limit: 1, type: 72});
-			let deletionLog = fetchedLogs.entries.first();
-			if (!deletionLog) {return console.log(`A message by ${message.author.tag} was deleted, but no relevant audit logs were found.`)}
-			// Now grab the user object of the person who deleted the message / Also grab the target of this action to double-check things
-			let { executor, target } = deletionLog;
-			// Update the output with a bit more information / Also run a check to make sure that the log returned was for the same author's message
-			if (target.id === message.author.id) { console.log(` deleted by ${executor.tag}.`); } 
-			else { console.log(` deleted, but we don't know by who, probably themselves.`); }
-			
-			let messageLogs = client.settings.get(message.guild.id, "messageLogs");
-			if(messageLogs) { 
-				if (client.channels.cache.get(client.settings.get(message.guild.id, "moderationChannel"))) {channel = client.channels.cache.get(client.settings.get(message.guild.id, "moderationChannel"))} else {channel = message.guild.systemChannel}
-				channel.send({
-					content: `Message deleted in <#${message.channelId}> (${message.author.toString()}) => 
+			try{
+				// Due to v14 Message_Delete type is now number 72
+				let fetchedLogs = await message.guild.fetchAuditLogs({ limit: 1, type: 72});
+				let deletionLog = fetchedLogs.entries.first();
+				if (!deletionLog) {return console.log(`A message by ${message.author.tag} was deleted, but no relevant audit logs were found.`)}
+				// Now grab the user object of the person who deleted the message / Also grab the target of this action to double-check things
+				let { executor, target } = deletionLog;
+				// Update the output with a bit more information / Also run a check to make sure that the log returned was for the same author's message
+				if (target.id === message.author.id) { console.log(` deleted by ${executor.tag}.`); } 
+				else { console.log(` deleted, but we don't know by who, probably themselves.`); }
+				
+				let messageLogs = client.settings.get(message.guild.id, "messageLogs");
+				if(messageLogs) { 
+					if (client.channels.cache.get(client.settings.get(message.guild.id, "moderationChannel"))) {channel = client.channels.cache.get(client.settings.get(message.guild.id, "moderationChannel"))} else {channel = message.guild.systemChannel}
+					channel.send({
+						content: `Message deleted in <#${message.channelId}> (${message.author.toString()}) => 
 "${message.content}"`
 + (deletionLog ?  (target.id === message.author.id ? `\ndeleted by ${executor.tag}. ` : `\nWe don't know by who, probably themselves. `) : `\nNo relevant audit logs were found. `)
 + "\n✅ "
 + (message.embeds.length?"Embed detected ":"") + (message.attachments.size?"Attachment detected ":"") + (message.components.length?"Components detected ":"") + (message.stickers.size?"Stickers detected":""),
-				});
-				embed.data.fields ? channel.send({ embeds: [embed] }) : null
+					});
+					embed.data.fields ? channel.send({ embeds: [embed] }) : null
+				}
+			} catch {
+				console.log(`[${new Date().toLocaleString('hu-HU')}] messageDelete - Not enough permission for ${message.guild.name}. Continuing...`)
 			}
 		} catch(error) { 
 			console.log("message Delete Error")
