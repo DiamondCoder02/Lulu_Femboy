43225089c981b5a591a27b97ba3cb6ac3d613da6
DiamondPRO02 <kovacsdanielmark@gmail.com>
2023-07-15 21:22:30 +0200

    It's hard to be productive

--- dashboard/server.js
+++ dashboard/server.js
@@ -8,13 +8,25 @@ app.set("view engine", "pug");
 app.use(express.static(`${__dirname}/assets`));
 app.locals.basedir = `${__dirname}/assets`;
 
-app.get("/lulu", (req, res) => { res.render("main", {
+app.get("/lulu", (req, res) => res.render("main", {
 	botName: "Lulu",
 	message: "Lulu is a good girl!",
 	subtitle: "main page"
-});});
+}));
 
-app.all("*", (req, res) => res.render("errors/404"));
+app.get("/lulu/commands", (req, res) => res.render("pages/commands", {
+	botName: "Lulu",
+	subtitle: "commands"
+}));
+
+app.get("/lulu/teapot", (req, res) => res.render("errors/418", {
+	botName: "Lulu",
+	subtitle: "teapot"
+}));
+app.all("*", (req, res) => res.render("errors/404", {
+	botName: "Lulu",
+	subtitle: "error"
+}));
 
 const port = process.env.serverPort ||3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
